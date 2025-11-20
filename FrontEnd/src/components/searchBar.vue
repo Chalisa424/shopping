e6ac989aff48727066aa3a4b2108a905be3f623b
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-11-20 09:23:11 +0700

    Add searchBar

---
+++ FrontEnd/src/components/searchBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- Search -->
+    <div class="mb-6 flex justify-center">
+      <div class="relative w-full max-w-xl">
+        <input
+          :value="modelValue"
+          @input="$emit('update:modelValue',($event.target as HTMLInputElement).value)"
+          type="text"
+          :placeholder="placeholder"
+          class="w-full rounded-full border border-slate-300 px-4 py-2.5 pl-12 text-sm outline-none focus:border-emerald-400 focus:ring-2 focus:ring-emerald-300"
+        />
+        <!-- ปุ่มค้นหา -->
+        <button 
+        type="button"
+        class="absolute right-2 top-1/2 -translate-y-1/2 flex items-center justify-center
+             h-8 w-9 rounded-full bg-gradient-to-r from-indigo-500 to-emerald-400
+             shadow-md hover:opacity-90 transition"
+        @click="$emit('search')"
+             >
+        <Icon
+          icon="material-symbols:search-rounded"
+          width="20"
+          height="20"
+          class="text-white"
+        />
+        </button>
+      </div>
+    </div> 
+</template>
+
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+
+defineProps<{
+    modelValue: string
+    placeholder?: string
+}>()
+
+defineEmits<{
+    (e: 'update:modelValue', value: string): void
+    (e: 'search'):void
+}>()
+
+</script>
--- FrontEnd/src/views/ProductView.vue
+++ FrontEnd/src/views/ProductView.vue
@@ -6,29 +6,12 @@
     </h1>
 
     <!-- Search -->
-    <div class="mb-6 flex justify-center">
-      <div class="relative w-full max-w-xl">
-        <input
-          v-model="search"
-          type="text"
-          placeholder="ค้นหาสินค้า"
-          class="w-full rounded-full border border-slate-300 px-4 py-2.5 pl-12 text-sm outline-none focus:border-emerald-400 focus:ring-2 focus:ring-emerald-300"
-          @click="console.log('Search:', search)"
-        />
-        <button 
-        type="button"
-        class="absolute right-2 top-1/2 -translate-y-1/2 flex items-center justify-center
-             h-8 w-9 rounded-full bg-gradient-to-r from-indigo-500 to-emerald-400
-             shadow-md hover:opacity-90 transition"
-        >
-        <Icon
-          icon="material-symbols:search-rounded"
-          width="20"
-          height="20"
-          class="text-white"
-        />
-        </button>
-      </div>
+    <div class="mb-6 ">
+      <searchBar 
+      v-model="search"
+      placeholder="ค้นหาสินค้า"
+      @search="handleSearchClick"
+      />
     </div>
 
     <!-- loading / error -->
@@ -60,8 +43,8 @@
 </template>
 
 <script setup lang="ts">
-import { Icon } from "@iconify/vue";
 import pokemonCard from "../components/pokemonCard.vue";
+import SearchBar from "../components/searchBar.vue";
 import { fetchPokemonPage } from "../services/pokemon.service";
 import type { PokemonModel } from "../models/pokemon.model";
 import { ref, computed, onMounted } from "vue";
@@ -86,11 +69,16 @@ onMounted(async () => {
 
 const filteredPokemons = computed(() => {
   const keyword = search.value.trim().toLowerCase();
-  if (!keyword) return pokemons.value;
-  return pokemons.value.filter((p) => p.name.toLowerCase().includes(keyword));
+  if (!keyword) return pokemons.value
+  return pokemons.value.filter((p) => 
+  p.name.toLowerCase().includes(keyword));
 });
 
 const handleAddToCart = (p: PokemonModel) => {
   console.log("add to cart:", p.name);
 };
+
+const handleSearchClick = () =>{
+    console.log('search click:', search.value)
+}
 </script>
